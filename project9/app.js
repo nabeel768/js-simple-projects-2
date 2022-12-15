9869bb65aedf148fb1f60c93a58cbcb8d41430a8
Nabeel Khan <nabeel.kay@gmail.com>
2022-12-15 10:33:56 +0000

    added new exercise

---
+++ project9/app.js
@@ -0,0 +1,39 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0].addEventListener("click", function(){
+  window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=18")
+})
+document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener("click", function(){
+  window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=18")
+})
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click", function(){
+  window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=18")
+})
+document.getElementsByClassName("article")[0].addEventListener("click", function(){
+  window.open("https://soundcloud.com/prospect-js")
+})
+document.getElementsByClassName("article")[1].addEventListener("click", function(){
+  window.open("https://soundcloud.com/prospect-js")
+})
+document.getElementsByClassName("article")[2].addEventListener("click", function(){
+  window.open("https://soundcloud.com/prospect-js")
+})
+
+const cardRed=document.getElementsByClassName("card")[0]
+const cardOrange=document.getElementsByClassName("card")[1]
+const cardYellow=document.getElementsByClassName("card")[2]
+const cardGreen=document.getElementsByClassName("card")[3]
+const cardBlue=document.getElementsByClassName("card")[4]
+const cardPurple=document.getElementsByClassName("card")[5]
+
+cardRed.style.background="red"
+cardRed.style.color="white"
+cardOrange.style.background="orange"
+cardOrange.style.color="white"
+cardYellow.style.background="yellow"
+cardYellow.style.color="white"
+cardGreen.style.background="green"
+cardGreen.style.color="white"
+cardBlue.style.background="blue"
+cardBlue.style.color="white"
+cardPurple.style.background="purple"
+cardPurple.style.color="white"
+
